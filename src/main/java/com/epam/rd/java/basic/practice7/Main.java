f1b8fcbf3f570a827dc13e9735727668707eb861
jMushmyH <jMushmyH@email.com>
2020-09-02 19:39:51 +0300

    Revert "Merge branch 'master' of https://rep.lab.epam.com/api_client/jmushmyh-task7"
    This reverts commit ca11fc478d6b1de66ac2b643e51a06887db4fc03, reversing
    changes made to 60995459113451796effdabe62e3b9a557305c51.

--- src/main/java/com/epam/rd/java/basic/practice7/Main.java
+++ src/main/java/com/epam/rd/java/basic/practice7/Main.java
@@ -1,15 +1,8 @@
 package com.epam.rd.java.basic.practice7;
 
-import com.epam.rd.java.basic.practice7.entity.Bank;
-import com.epam.rd.java.basic.practice7.entity.Deposit;
-
 public final class Main {
 
-	public static void main(final String[] args) {
-		Bank bank = new Bank();
-		Deposit deposit = new Deposit();
-		bank.getDeposits();
-		bank.toString();
+    public static void main(final String[] args) {
 
-	}
+    }
 }
