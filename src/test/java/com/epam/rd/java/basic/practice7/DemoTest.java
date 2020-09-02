a2058dc32799f73cac4aaca8eb8d36fa8e24423e
jMushmyH <jMushmyH@email.com>
2020-09-03 02:41:28 +0300

    DOMController is done

--- src/test/java/com/epam/rd/java/basic/practice7/DemoTest.java
+++ src/test/java/com/epam/rd/java/basic/practice7/DemoTest.java
@@ -1,7 +1,12 @@
 package com.epam.rd.java.basic.practice7;
 
+import com.epam.rd.java.basic.practice7.constants.Constants;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import static org.junit.Assert.*;
 
@@ -11,13 +16,14 @@ import static org.junit.Assert.*;
 public class DemoTest {
 
     @Test
-    public void demoTestPlaceholder() {
+    public void demoTestPlaceholder() throws IOException {
         try {
             Demo.main(null);
         } catch (Exception e) {
             fail();
         }
         Assert.assertTrue(true);
+        Files.delete(Paths.get(Constants.DOM_OUTPUT_XML_FILE));
 
     }
 
