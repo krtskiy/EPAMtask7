5ed95f3d0709f877cab8875d2af02291d7a2fee4
jMushmyH <jMushmyH@email.com>
2020-09-02 17:58:40 +0300

    my

--- src/main/java/com/epam/rd/java/basic/practice7/Demo.java
+++ src/main/java/com/epam/rd/java/basic/practice7/Demo.java
@@ -1,15 +1,8 @@
 package com.epam.rd.java.basic.practice7;
 
-/**
- * Demo class to run project WO command line.
- * 
- * @author D.Kolesnikov
- * 
- */
-public class Demo {
-	
-	public static void main(String[] args) throws Exception {
+public final class Demo {
+
+	public static void main(String[] args) throws Exception { // NOSONAR
 		Main.main(new String[] { "input.xml" });
 	}
-	
 }
