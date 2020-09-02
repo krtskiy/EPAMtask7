8bc0cfbb53bd604b61428328e3b74feebbe6c8c7
jMushmyH <jMushmyH@email.com>
2020-09-02 19:48:04 +0300

    second commit

--- src/main/java/com/epam/rd/java/basic/practice7/Demo.java
+++ src/main/java/com/epam/rd/java/basic/practice7/Demo.java
@@ -1,8 +1,24 @@
 package com.epam.rd.java.basic.practice7;
 
+<<<<<<< HEAD
 public final class Demo {
 
     public static void main(String[] args) throws Exception {
         Main.main(new String[] { "input.xml" });
     }
 }
+=======
+/**
+ * Demo class to run project WO command line.
+ * 
+ * @author D.Kolesnikov
+ * 
+ */
+public class Demo {
+	
+	public static void main(String[] args) throws Exception {
+		Main.main(new String[] { "input.xml" });
+	}
+	
+}
+>>>>>>> parent of 5ed95f3... my
